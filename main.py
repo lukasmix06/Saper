3f02cf005aca2bb4551d394d9ab4c7d011b8835c
lukasmix06 <34247465+lukasmix06@users.noreply.github.com>
2020-06-20 09:59:34 +0200

    Zaczątek klasy Przycisk

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-while(1):
-    n = int(input("Wprowadź pierwszy wymiar planszy: 5"))
+while 1:
+    n = int(input("Wprowadź pierwszy wymiar planszy: "))
     if n >= 2 and n <= 15:
         break
     else:
         print("Niewlasciwy rozmiar! - musi miescic się w zakresie [2 - 15]")
 
-while(1):
+while 1:
     m = int(input("Wprowadź drugi wymiar planszy: "))
     if m >= 2 and m <= 15:
         break
     else:
         print("Niewlasciwy rozmiar! - musi miescic się w zakresie [2 - 15]")
 
-while(1):
-    lMin = int(input("Wprowadź liczbę min: "))
-    if lMin >= 0 and lMin < n*m:
+while 1:
+    l_min = int(input("Wprowadź liczbę min: "))
+    if l_min >= 0 and l_min < n*m:
         break
     else:
         print("Niewlasciwa liczba min - musi byc nieujemna i mniejsza od calkowitej liczby pol")
+
+l_flag = 0
+l_pozostalych_min = l_min
+while 1:
+    print("liczba oznaczonych pól: {}\nliczba pozostałych min: {}".format(l_flag, l_pozostalych_min))
+    napis = 'Rozpocznij grę'
+    break
+    #przycisk rozpoczęcia nowej gry(element interfejsu, z napisem string "Rozpocznij!")
+
+class Przycisk:
+    def __init__(self):
+        czybomba = True
+
 
