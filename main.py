735186a503314879e7a1ad1f84868491716dd80d
lukasmix06 <34247465+lukasmix06@users.noreply.github.com>
2020-06-21 20:01:51 +0200

    Rozwinięcie pętli głównej programu, w tym wstępnej opcji klikania na pola planszy

--- main.py
+++ main.py
@@ -21,44 +21,62 @@ while 1:
     else:
         print("Niewlasciwa liczba min - musi byc nieujemna i mniejsza od calkowitej liczby pol")
 
-l_flag = 0
-l_pozostalych_min = l_min
-while 1:
-    print("liczba oznaczonych pól: {}\nliczba pozostałych min: {}".format(l_flag, l_pozostalych_min))
-    napis = 'Rozpocznij grę'
-    break
-    #przycisk rozpoczęcia nowej gry(element interfejsu, z napisem string "Rozpocznij!")
-
 class Przycisk:
     def __init__(self):
         self.bomba = False
         self.sasiednie_bomby = 0 #trzeba to jakoś zliczać, jeszcze nie wiem czy to ma być na poziomie przycisku czy planszy
         self.flaga = False
         self.pytajnik = False
-        self.zaznaczony = False #nie wiem czy to w ogóle jest potrzebne
+        self.widoczny = False #nie wiem czy to w ogóle jest potrzebne
 
 class Plansza:
     def __init__(self, n, m):
-        self.tab_przyciskow = [[Przycisk() for i in range(n)] for j in range(m)]
+        self.tab_przyciskow = [[Przycisk() for i in range(m)] for j in range(n)]
 
-    def kliknij(self, x, y): #x i y to położenie przycisku
-        pass
+    def kliknij(self):
+        odczyt = input().split()
+        tryb = odczyt[0]
+        wspolrzedne = int(odczyt[1]), int(odczyt[2])
+        return tryb, wspolrzedne
 
     def rozstaw_miny(self, l_min, x, y):
-        poz_pierwsza = x + y #Źle to jest - poprawić!!!
-        lista_bez_pierwszej = list(range((n*m)-1)).remove(poz_pierwsza)
-        bombowa_lista = random.sample(lista_bez_pierwszej, l_min)
+        poz_pierwsza = x*m + y
+        pozycje_do_losowania = list(range(n*m))
+        pozycje_do_losowania.remove(poz_pierwsza) #usunięcie pierwszej zaznaczonej pozycji
+        print(pozycje_do_losowania)
+        bombowa_lista = random.sample(pozycje_do_losowania, l_min)
         bombowa_lista_wspolrzedne = [(bombowa_lista[i]//m, bombowa_lista[i]%m) for i in range(l_min)]
+        print(bombowa_lista)
+        print(bombowa_lista_wspolrzedne)
         for i, j in bombowa_lista_wspolrzedne:
             self.tab_przyciskow[i][j].bomba = True
 
+l_flag = 0
+l_pozostalych_min = l_min
+#napis = 'ROZPOCZNIJ GRĘ'
+print('Rozpocznij grę')
+#przycisk rozpoczęcia nowej gry(element interfejsu, z napisem string "Rozpocznij!")
+nowa_plansza = Plansza(n, m)
+rozstawione = 0
+
 while 1: #PĘTLA GRY
-    #napis = 'Rozpocznij grę'
-    #przycisk rozpoczęcia nowej gry(element interfejsu, z napisem string "Rozpocznij!")
-    nowa_plansza = Plansza(n, m)
-    nowa_plansza.kliknij(4, 4)
-    nowa_plansza.rozstaw_miny(l_min, 4, 4)
     print("liczba oznaczonych pól: {}\nliczba pozostałych min: {}".format(l_flag, l_pozostalych_min))
+    tryb, wspolrzedne = nowa_plansza.kliknij()
+    wskazane_pole = Plansza.tab_przyciskow[wspolrzedne[0]][wspolrzedne[1]]
+    if tryb == 'k' #kliknięcie na konkretne pole
+        if rozstawione == 0: #jeśli to pierwsze naciśnięte pole to rozstaw na reszcie pól planszy bomby
+            nowa_plansza.rozstaw_miny(l_min, *wspolrzedne)
+            rozstawione = 1
+        if wskazane_pole.bomba == True: #Jeśli w polu jest bomba:
+            print(BOMBA - PRZEGRANA!)
+            break
+        else: #Jeśli w polu nie ma bomby:
+            wskazane_pole.widoczne = True
+    if tryb == 'f': #ustawienie flagi na polu
+        wskazane_pole.flaga = True
+        l_flag += 1
+        l_pozostalych_min -= 1
+    if tryb == 'p' #ustawienie znaku zapytania
     break
 
 
